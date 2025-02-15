9b2c9ca402f2adee977ac5089c5b8969658ff2ae
MilkClouds <milkclouds00@gmail.com>
2025-02-16 02:33:50 +0900

    feat(gst): added recorder

--- projects/owa-env-gst/owa_env_gst/__init__.py
+++ projects/owa-env-gst/owa_env_gst/__init__.py
@@ -1,10 +1,12 @@
-# This module contains listeners and callables for desktop environment.
-# Components:
-#    - screen
-#    - keyboard_mouse
-#    - window
+import os
+
+# set GST_PLUGIN_PATH to the 'gst-plugins' directory in the current working directory
+os.environ["GST_PLUGIN_PATH"] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "gst-plugins")
+
+
 from . import gst_factory
 
 
 def activate():
     from . import screen  # noqa
+    from . import recorder  # noqa
