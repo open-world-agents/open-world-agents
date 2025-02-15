83bfcaacd9237c0078fce926aa73a2a4346641c0
MilkClouds <milkclouds00@gmail.com>
2025-02-16 03:04:46 +0900

    feat: added example env package

--- projects/core/owa/callable.py
+++ projects/core/owa/callable.py
@@ -3,3 +3,8 @@ Callable implements `__call__` method, which allows the object to be called as a
 """
 
 from typing import Callable
+
+
+class CallableMixin:
+    def __call__(self):
+        raise NotImplementedError
