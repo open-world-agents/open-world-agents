6a4e643e43184928b866ce9190758d473f62ed5c
MilkClouds <milkclouds00@gmail.com>
2025-02-21 15:45:17 +0900

    fix: fix gst-python version issue

--- projects/owa-env-gst/owa_env_gst/__init__.py
+++ projects/owa-env-gst/owa_env_gst/__init__.py
@@ -1,16 +1,17 @@
 import os
+import subprocess
 
+# check if GStreamer is properly installed
 try:
-    os.system("gst-inspect-1.0.exe d3d11")
+    subprocess.run(["gst-inspect-1.0.exe", "d3d11"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 except Exception as e:  # noqa: F841
     raise ImportError(
-        "GStreamer is not properly installed or not in PATH."
+        "GStreamer is not properly installed or not in PATH. "
         "Please install conda packages in `projects/owa-env-gst/environment_detail.yml`"
     )
 
 # set GST_PLUGIN_PATH to the 'gst-plugins' directory in the current working directory
 os.environ["GST_PLUGIN_PATH"] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "gst-plugins")
-
 
 from . import gst_factory
 
