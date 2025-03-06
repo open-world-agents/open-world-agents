b666dc8ad41384d4027830a71eebd201e9fae3c2
MilkClouds <milkclouds00@gmail.com>
2025-03-06 10:49:59 +0900

    chore: moved msg definition to `project-name/msg.py`

--- projects/owa-env-desktop/owa_env_desktop/keyboard_mouse/__init__.py
+++ projects/owa-env-desktop/owa_env_desktop/keyboard_mouse/__init__.py
@@ -3,8 +3,3 @@ from . import callables  # noqa
 
 # Register listeners
 from . import listeners  # noqa
-
-from .msg import KeyboardEvent, MouseEvent
-
-
-__all__ = ["KeyboardEvent", "MouseEvent"]
--- projects/owa-env-desktop/owa_env_desktop/window/__init__.py
+++ projects/owa-env-desktop/owa_env_desktop/window/__init__.py
@@ -1,6 +1,2 @@
 # Register callables
 from . import callables  # noqa
-from .msg import WindowInfo
-
-
-__all__ = ["WindowInfo"]
