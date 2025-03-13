fafcb7464f0206dfe544da6490f4a2dc234ca4c5
MilkClouds <milkclouds00@gmail.com>
2025-03-13 13:41:30 +0900

    feat: apply namespace package to example env

--- projects/owa-env-example/tests/test_print.py
+++ projects/owa-env-example/tests/test_print.py
@@ -6,7 +6,7 @@ from owa.core.registry import CALLABLES, activate_module
 # Automatically activate the desktop module for all tests in this session.
 @pytest.fixture(scope="session", autouse=True)
 def activate_owa_desktop():
-    activate_module("owa_env_example")
+    activate_module("owa.env.example")
 
 
 def test_screen_capture():
