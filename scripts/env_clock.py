248581f6570508c2ad8a539cbe5b6cdbbbf96b3c
Haebin Seong <hbseong97@gmail.com>
2025-03-25 13:43:19 +0900

    chore: lint example code

--- scripts/env_clock.py
+++ scripts/env_clock.py
@@ -6,10 +6,14 @@ print(CALLABLES, LISTENERS)  # {}, {}
 
 # Activate the standard module to register clock functionalities
 activate_module("owa.env.std")
-print(CALLABLES, LISTENERS) # {'clock.time_ns': <built-in function time_ns>} {'clock/tick': <class 'owa.env.std.clock.ClockTickListener'>}
+print(
+    CALLABLES, LISTENERS
+)  # {'clock.time_ns': <built-in function time_ns>} {'clock/tick': <class 'owa.env.std.clock.ClockTickListener'>}
 
 # Testing the clock/tick listener
-tick = LISTENERS["clock/tick"]().configure(callback=lambda: print(CALLABLES["clock.time_ns"]()), interval=1)
+tick = LISTENERS["clock/tick"]().configure(
+    callback=lambda: print(CALLABLES["clock.time_ns"]()), interval=1
+)
 tick.start()
 
 time.sleep(2)  # The listener prints the current time in nanoseconds a few times
