c6eb8fdcf9f96c9dcfa5759b6713c62e4387068e
MilkClouds <milkclouds00@gmail.com>
2025-06-08 23:23:11 +0900

    fix: update GStreamer installation check to use logger for warnings

--- projects/owa-env-gst/owa/env/gst/__init__.py
+++ projects/owa-env-gst/owa/env/gst/__init__.py
@@ -9,6 +9,8 @@ import os
 import platform
 import subprocess
 
+from loguru import logger
+
 # check if GStreamer is properly installed. TODO: multi-os support
 try:
     # if os is windows
@@ -19,7 +21,7 @@ try:
     elif platform.system() == "Darwin":
         subprocess.run(["gst-inspect-1.0", "avfvideosrc"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 except Exception as e:  # noqa: F841
-    raise ImportError(
+    logger.warning(
         "GStreamer is not properly installed or not in PATH. "
         "Please run `conda install open-world-agents::gstreamer-bundle`"
     )
