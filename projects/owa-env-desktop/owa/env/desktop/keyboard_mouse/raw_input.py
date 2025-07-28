cc2fe4093e88dd6dd352f77ef047e542b3c1ae7a
Haebin Seong <hbseong97@gmail.com>
2025-07-28 15:04:45 +0900

    fix: fix rawmouse struct for correct offset

--- projects/owa-env-desktop/owa/env/desktop/keyboard_mouse/raw_input.py
+++ projects/owa-env-desktop/owa/env/desktop/keyboard_mouse/raw_input.py
@@ -30,18 +30,7 @@ if sys.platform == "win32":
     HID_USAGE_GENERIC_MOUSE = 0x02
 
     # Raw mouse button flags
-    RI_MOUSE_LEFT_BUTTON_DOWN = 0x0001
-    RI_MOUSE_LEFT_BUTTON_UP = 0x0002
-    RI_MOUSE_RIGHT_BUTTON_DOWN = 0x0004
-    RI_MOUSE_RIGHT_BUTTON_UP = 0x0008
-    RI_MOUSE_MIDDLE_BUTTON_DOWN = 0x0010
-    RI_MOUSE_MIDDLE_BUTTON_UP = 0x0020
-    RI_MOUSE_BUTTON_4_DOWN = 0x0040
-    RI_MOUSE_BUTTON_4_UP = 0x0080
-    RI_MOUSE_BUTTON_5_DOWN = 0x0100
-    RI_MOUSE_BUTTON_5_UP = 0x0200
-    RI_MOUSE_WHEEL = 0x0400
-    RI_MOUSE_HWHEEL = 0x0800
+    RAWMOUSE_BUTTON_FLAGS = RawMouseEvent.ButtonFlags
 
     # Windows structures
     class RAWINPUTDEVICE(Structure):
@@ -60,11 +49,22 @@ if sys.platform == "win32":
             ("wParam", wintypes.WPARAM),
         ]
 
+    class RAWMOUSE_BUTTONS(Structure):
+        _fields_ = [
+            ("usButtonFlags", wintypes.USHORT),
+            ("usButtonData", wintypes.USHORT),
+        ]
+
+    class RAWMOUSE_BUTTONS_UNION(Union):
+        _fields_ = [
+            ("ulButtons", wintypes.ULONG),
+            ("Buttons", RAWMOUSE_BUTTONS),
+        ]
+
     class RAWMOUSE(Structure):
         _fields_ = [
             ("usFlags", wintypes.USHORT),
-            ("usButtonFlags", wintypes.USHORT),
-            ("usButtonData", wintypes.USHORT),
+            ("ButtonsUnion", RAWMOUSE_BUTTONS_UNION),
             ("ulRawButtons", wintypes.ULONG),
             ("lLastX", wintypes.LONG),
             ("lLastY", wintypes.LONG),
@@ -365,8 +365,8 @@ if sys.platform == "win32":
                 event = RawMouseEvent(
                     dx=mouse_data.lLastX,
                     dy=mouse_data.lLastY,
-                    button_flags=mouse_data.usButtonFlags,
-                    button_data=mouse_data.usButtonData,
+                    button_flags=mouse_data.ButtonsUnion.Buttons.usButtonFlags,
+                    button_data=mouse_data.ButtonsUnion.Buttons.usButtonData,
                     device_handle=int(raw_input.header.hDevice) if raw_input.header.hDevice else None,
                     timestamp=time.time_ns(),
                 )
