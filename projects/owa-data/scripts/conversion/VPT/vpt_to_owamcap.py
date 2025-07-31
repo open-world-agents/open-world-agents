0ddf5d96dec6e5dd829719383d7ef817205490e2
Haebin Seong <hbseong97@gmail.com>
2025-07-31 23:57:59 +0000

    fix: change absolute path to relative in mediaref

--- projects/owa-data/scripts/conversion/VPT/vpt_to_owamcap.py
+++ projects/owa-data/scripts/conversion/VPT/vpt_to_owamcap.py
@@ -167,7 +167,7 @@ def process_single_file(jsonl_file_path):
             utc_ns=unix_epoch_ns,
             source_shape=(VPT_X_RESOLUTION, VPT_Y_RESOLUTION),
             shape=(VPT_X_RESOLUTION, VPT_Y_RESOLUTION),
-            media_ref=MediaRef(uri=str(mp4_file_path), pts_ns=unix_epoch_ns),
+            media_ref=MediaRef(uri=str(mp4_file_path.name), pts_ns=unix_epoch_ns),
         )
         writer.write_message(event, topic=topic, timestamp=unix_epoch_ns)
 
@@ -181,7 +181,7 @@ def process_single_file(jsonl_file_path):
                 utc_ns=log_time,
                 source_shape=(VPT_X_RESOLUTION, VPT_Y_RESOLUTION),
                 shape=(VPT_X_RESOLUTION, VPT_Y_RESOLUTION),
-                media_ref=MediaRef(uri=str(mp4_file_path), pts_ns=log_time),
+                media_ref=MediaRef(uri=str(mp4_file_path.name), pts_ns=log_time),
             )
             writer.write_message(event, topic=topic, timestamp=log_time)
 
